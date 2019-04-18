1d0a106eebc7594c7b805b912070fc217d53ab39
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-04-18 14:45:08 -0400

    decoupled rank and suit from card

--- src/main/java/com/zipcodewilmington/bakery/models/cardgames/utils/Card.java
+++ src/main/java/com/zipcodewilmington/bakery/models/cardgames/utils/Card.java
@@ -1,58 +1,6 @@
 package com.zipcodewilmington.bakery.models.cardgames.utils;
 
-import javax.persistence.Entity;
-
-@Entity
 public class Card {
-
-    enum Rank {
-        LOW_ACE(1),
-        TWO(2),
-        THREE(3),
-        FOUR(4),
-        FIVE(5),
-        SIX(6),
-        SEVEN(7),
-        EIGHT(8),
-        NINE(9),
-        TEN(10),
-        JACK(11),
-        QUEEN(12),
-        KING(13);
-
-        private final int value;
-
-        Rank(int value) {
-            this.value = value;
-        }
-
-        public int getValue() {
-            return this.value;
-        }
-    }
-
-    enum Suit {
-        HEARTS("♥"),
-        DIAMONDS("♦"),
-        CLUBS("♣"),
-        SPADES("♠");
-
-        private String cardGraphic;
-
-        Suit(String cardGraphic) {
-            this.cardGraphic = cardGraphic;
-        }
-
-        public String getCardGraphic() {
-            return cardGraphic;
-        }
-
-        public static int length() {
-            return values().length;
-        }
-    }
-
-
     private final Rank rank;
     private final Suit suit;
 
--- src/main/java/com/zipcodewilmington/bakery/models/cardgames/utils/Suit.java
+++ src/main/java/com/zipcodewilmington/bakery/models/cardgames/utils/Suit.java
@@ -1,4 +1,22 @@
 package com.zipcodewilmington.bakery.models.cardgames.utils;
 
-public class Suit {
+public enum Suit {
+    HEARTS("♥"),
+    DIAMONDS("♦"),
+    CLUBS("♣"),
+    SPADES("♠");
+
+    private String cardGraphic;
+
+    Suit(String cardGraphic) {
+        this.cardGraphic = cardGraphic;
+    }
+
+    public String getCardGraphic() {
+        return cardGraphic;
+    }
+
+    public static int length() {
+        return values().length;
+    }
 }
